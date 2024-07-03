a0c399f586837a6177fc768097d242a9395c0926
ramemes <17809655+ramemes@users.noreply.github.com>
2024-07-03 17:20:35 +0100

    scoping data to user identifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated } from "convex/react";
+import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react";
 import Image from "next/image";
 
 export default function Home() {
+
+  const documents = useQuery(api.documents.getDocuments)
+  const createDocument = useMutation(api.documents.createDocument)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Unauthenticated>
@@ -12,6 +17,16 @@ export default function Home() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+        <button
+          onClick={() => createDocument({title: "Hello world"})}
+        >Click Me</button>
+
+        {documents?.map((document) => (
+          <div key={document._id} className="flex items-center justify-center">
+            <h1>{document.title}</h1>
+          </div>
+        
+        ))}
       </Authenticated>
     </main>
   );
